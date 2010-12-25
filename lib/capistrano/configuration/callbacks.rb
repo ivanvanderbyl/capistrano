d6a8382f803af8b737a4a927dac7b0a242f1f99c
Ivan Vanderbyl <ivanvanderbyl@me.com>
2010-12-25 18:52:00 +1100

    Trying some new things

--- lib/capistrano/configuration/callbacks.rb
+++ lib/capistrano/configuration/callbacks.rb
@@ -125,6 +125,7 @@ module Capistrano
           msg = "triggering #{event} callbacks"
           msg << " for `#{task.fully_qualified_name}'" if task
           logger.trace(msg)
+          puts "TASK: #{task}"
           pending.each { |callback| callback.call }
         end
       end
